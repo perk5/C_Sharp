50a30d4576debe7419965b9b89d0f53051628ad4
Prerak Shah <prerak9359@gmail.com>
2025-08-26 15:01:54 +0930

    Working with Attributes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,36 @@
 ﻿using System;
-
+using System.Collections.Generic;
 public class MainClass
 {
-    public static void Main()
+    private static void Main()
     {
-        //Console.WriteLine("Hello World..");
+        //Calculator.Add(4, 5);
+
+
+
+
+        //List<int> numLIst = new List<int> { 4, 5, 15, 20, 30};
+        int Add = Calculator.Add(new List<int> { 4, 5, 15, 20, 30 });
+
+        Console.WriteLine(Add);
+
+    }
+}
+
+public class Calculator
+{
+    [ObsoleteAttribute("Use Add(List<int> Numbers) Method")]
+    public static int Add(int Fn, int Sn)
+    {
+        return Fn + Sn;
+    }
+    public static int Add(List<int> Numbers)
+    {
+        int Add = 0;
+        foreach(int n in Numbers)
+        {
+            Add += n;
+        }
+        return Add;
     }
 }
