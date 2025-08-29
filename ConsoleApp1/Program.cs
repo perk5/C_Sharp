53a9ebe0f82760a5e519817773f31501861f6e86
Prerak Shah <prerak9359@gmail.com>
2025-08-29 12:47:04 +0930

    Overriding Equals and GetHashCode -> System.Objects

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,25 +6,51 @@ namespace Prerak
     {
         private static void Main()
         {
-            Customer C1 = new Customer();
+            //int i = 10;
+            //int j = 10;
+            //Console.WriteLine(i == j);
+            //Console.WriteLine(i.Equals(j));
 
+            Customer C1 = new Customer();
             C1.FirstName = "Prerak";
             C1.LastName = "Shah";
 
-            Console.WriteLine(C1.ToString());
+            Customer C2 = new Customer();
+            C2.FirstName = "Prerak";
+            C2.LastName = "Shah";
+
+            Console.WriteLine(C1 == C2);
+            Console.WriteLine(C2.Equals(C1));
+
+
+
         }
     }
 
     public class Customer
     {
-        
-        public string FirstName { get; set; }
+        public string FirstName {  get; set; }
         public string LastName { get; set; }
 
-        public override string ToString()
+
+        public override bool Equals(object obj)
         {
-            return this.FirstName + " " + this.LastName;  
+            if(obj == null)
+            {
+                return false;
+            }
+
+            if (!(obj is Customer)) 
+            { 
+                return false;
+            }
+
+            return this.FirstName == ((Customer)obj).FirstName && this.LastName == ((Customer)obj).LastName;
         }
-        
+
+        public override int GetHashCode()
+        {
+            return this.FirstName.GetHashCode() ^ this.LastName.GetHashCode();  
+        }
     }
 }
