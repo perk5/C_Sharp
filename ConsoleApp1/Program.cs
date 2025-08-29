8cbfdb399ad04e37615772cb0d17bce7f3c960d0
Prerak Shah <prerak9359@gmail.com>
2025-08-29 14:02:13 +0930

    Convert.ToString() vs String method....

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,51 +6,19 @@ namespace Prerak
     {
         private static void Main()
         {
-            //int i = 10;
-            //int j = 10;
-            //Console.WriteLine(i == j);
-            //Console.WriteLine(i.Equals(j));
-
-            Customer C1 = new Customer();
-            C1.FirstName = "Prerak";
-            C1.LastName = "Shah";
-
-            Customer C2 = new Customer();
-            C2.FirstName = "Prerak";
-            C2.LastName = "Shah";
-
-            Console.WriteLine(C1 == C2);
-            Console.WriteLine(C2.Equals(C1));
-
-
-
+            Customer C1 = null;
+            string str = Convert.ToString(C1);
+            //string str = C1.ToString(); -> Throws a nullReferenceError
+            Console.WriteLine(str); 
         }
     }
 
     public class Customer
     {
-        public string FirstName {  get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
 
-
-        public override bool Equals(object obj)
-        {
-            if(obj == null)
-            {
-                return false;
-            }
-
-            if (!(obj is Customer)) 
-            { 
-                return false;
-            }
-
-            return this.FirstName == ((Customer)obj).FirstName && this.LastName == ((Customer)obj).LastName;
-        }
-
-        public override int GetHashCode()
-        {
-            return this.FirstName.GetHashCode() ^ this.LastName.GetHashCode();  
-        }
+          
     }
+
+    
 }
