bc20cd57a6ae9b765e34da9409042fc1da0217fb
Prerak Shah <prerak9359@gmail.com>
2025-09-01 02:45:45 +0930

    Working with Dictionaries

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
 
 namespace Demo
 {
@@ -7,22 +8,48 @@ namespace Demo
     {
         public static void Main()
         {
-            Program.AddNumbers(1 , 2);
-        }
-
-        public static void AddNumbers(int firstNumber, int secondNumber, [Optional] int[] restOfNumbers)
-        {
-            int result = firstNumber + secondNumber;
-
-            if (restOfNumbers != null)
+            Customer customer1 = new Customer()
             {
-                foreach (int i in restOfNumbers)
-                {
-                    result += i;
-                }
-            }
+                ID = 1,
+                Name = "Test1",
+                Salary = 200
+            };
+            Customer customer2 = new Customer()
+            {
+                ID = 2,
+                Name = "Test2",
+                Salary = 300
+            };
+            Customer customer3 = new Customer()
+            {
+                ID = 3,
+                Name = "Test3",
+                Salary = 400
+            };
 
-            Console.WriteLine(result);
-        }
+            Dictionary<int, Customer> dictionaryCustomers = new Dictionary<int, Customer>();
+            dictionaryCustomers.Add(customer1.ID, customer1);
+            dictionaryCustomers.Add(customer2.ID, customer2);
+
+            Customer cust2 = dictionaryCustomers[2];
+
+            //Console.WriteLine("ID = {0}, Name = {1}, Salary = {2}", cust2.ID, cust2.Name, cust2.Salary);
+
+            foreach (KeyValuePair<int, Customer> customerKeyValuePair in dictionaryCustomers )
+            {
+                //Console.WriteLine("ID = {0}", customerKeyValuePair.Key);
+                Customer Cust = customerKeyValuePair.Value;
+                Console.WriteLine(Cust.Name);
+            }
+        } 
+    }
+
+    public class Customer
+    {
+        public  int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+
     }
 }
