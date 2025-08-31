3d410a033bbecbc02f52e7801aca581fa5c63960
Prerak Shah <prerak9359@gmail.com>
2025-08-31 13:23:38 +0930

    Making method parameters optional using method overloading

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,22 +2,31 @@
 
 namespace ConsoleApp1
 {
-    public class MainClass
+    class Program
     {
         public static void Main()
         {
-            Company C1 = new Company();
-            Console.WriteLine("First Employee Name = " + C1[1]);
-            Console.WriteLine("First Employee Name = " + C1[2]);
-            Console.WriteLine("First Employee Name = " + C1[3]);
+           
+            Program.AddNumbers(1, 2);
+        }
 
-            C1[1] = "Changed Prerak";
-            C1[2] = "Changed Raj";
-            C1[3] = "Changed Ram";
+        public static void AddNumbers(int firstNumber, int secondNumber)
+        {
+            AddNumbers(firstNumber, secondNumber, null);
+        }
 
-            Console.WriteLine("First Employee Name = " + C1[1]);
-            Console.WriteLine("First Employee Name = " + C1[2]);
-            Console.WriteLine("First Employee Name = " + C1[3]);
+        public static void AddNumbers(int firstNumber, int secondNumber, int[] restOfNumbers)
+        {
+            int result = firstNumber + secondNumber;
+            if(restOfNumbers != null)
+            {
+                foreach (int i in restOfNumbers)
+                {
+                    result += i;    
+                }
+            }
+
+            Console.WriteLine(result);  
         }
     }
 }
