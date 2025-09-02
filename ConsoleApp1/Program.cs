5f892c309fc2db3b1092ae3835663799cac7a28c
Prerak Shah <prerak9359@gmail.com>
2025-09-02 23:59:20 +0930

    Working with Stacks<>

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,28 +14,22 @@ namespace Prerak
             Country country4 = new Country() { Name = "Iran", Code = "IRN", Capital = "Tehran" };
             Country country5 = new Country() { Name = "Pakistan", Code = "PAK", Capital = "Islamabad" };
 
-            Queue<Country> queueCustomers = new Queue<Country>();
-            queueCustomers.Enqueue(country1);
-            queueCustomers.Enqueue(country2);
-            queueCustomers.Enqueue(country3);
-            queueCustomers.Enqueue(country4);
-            queueCustomers.Enqueue(country5);
+            Stack<Country> queueCustomers = new Stack<Country>();
+            queueCustomers.Push(country1);
+            queueCustomers.Push(country2);
+            queueCustomers.Push(country3);
+            queueCustomers.Push(country4);
+            queueCustomers.Push(country5);
 
-
-            //FIFO (First In First Out)
-            //Remove and just Return
-            //Country c1 = queueCustomers.Dequeue();
-            //Console.WriteLine(c1.Name);
-
-            //Doesnot Remove and just Return
-            Country c1 = queueCustomers.Peek();
-            Console.WriteLine(c1.Name);
-
+            queueCustomers.Pop();
 
             foreach (Country c in queueCustomers)
-            {  
-                Console.WriteLine(queueCustomers.Count());
+            {
+                Console.WriteLine(c.Name);
             }
+
+
+            
 
         }
     }
