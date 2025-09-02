ff2ff8a49e8d1456d40bcafd7b3ad52e644ab950
Prerak Shah <prerak9359@gmail.com>
2025-09-02 22:45:34 +0930

    Queues in C#

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,60 +14,22 @@ namespace Prerak
             Country country4 = new Country() { Name = "Iran", Code = "IRN", Capital = "Tehran" };
             Country country5 = new Country() { Name = "Pakistan", Code = "PAK", Capital = "Islamabad" };
 
-            Dictionary<string, Country> Countries = new Dictionary<string, Country>();
-            Countries.Add(country1.Code, country1);
-            Countries.Add(country2.Code, country2);
-            Countries.Add(country3.Code, country3);
-            Countries.Add(country4.Code, country4);
-            Countries.Add(country5.Code, country5);
-                 
-            string answer;
-            do
+            Queue<Country> queueCustomers = new Queue<Country>();
+            queueCustomers.Enqueue(country1);
+            queueCustomers.Enqueue(country2);
+            queueCustomers.Enqueue(country3);
+            queueCustomers.Enqueue(country4);
+            queueCustomers.Enqueue(country5);
+
+            //Country c1 = queueCustomers.Dequeue();
+            //Console.WriteLine(c1.Name);
+            foreach (Country c in queueCustomers)
             {
-                Console.WriteLine("Please enter country code: ");
-                string Code = Console.ReadLine().ToUpper();
+                queueCustomers.Dequeue();
+                Console.WriteLine(queueCustomers.Count());
 
+            }
 
-                Country c = Countries.ContainsKey(Code) ? Countries[Code] :  null;
-
-                if (c != null)
-                {
-                    Console.WriteLine(c.Name);
-                }
-                else
-                {
-                    Console.WriteLine("We were not able to find the name of the country..");
-                }
-                Console.WriteLine("");
-
-                do
-                {
-                    Console.Write("Do you want to continue Yes or No: ");
-                    answer = Console.ReadLine().ToLower();
-                } while (answer != "no" && answer != "yes");
-
-
-
-                //foreach (Country C in countries)
-                //{
-                //    if (C.Code == Code)
-                //    {
-                //        Console.WriteLine("Country: " + C.Name);
-
-                //        answer = Console.ReadLine();
-                //    }
-                //    else
-                //    {
-
-                //    }
-                //    Console.WriteLine("Do You want to continue Yes or No: ");
-                //}
-
-
-
-                } while (answer != "no");
-
-    
         }
     }
      
