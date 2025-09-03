d722d48a80a78a5b4661de0eaeb792795a4acd6a
Prerak Shah <prerak9359@gmail.com>
2025-09-04 01:34:14 +0930

    Working on Multithreads

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,43 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace Prerak
+namespace ThreadStartDelegateExample
 {
-    public class MainClass
+    class Program
     {
         public static void Main()
         {
-            Country country1 = new Country() { Name = "India", Code = "IND", Capital = "Delhi" };
-            Country country2 = new Country() { Name = "Australia", Code = "AUS", Capital = "Canberra" };
-            Country country3 = new Country() { Name = "Afghanistan", Code = "AFG", Capital = "Kabul" };
-            Country country4 = new Country() { Name = "Iran", Code = "IRN", Capital = "Tehran" };
-            Country country5 = new Country() { Name = "Pakistan", Code = "PAK", Capital = "Islamabad" };
-
-            Stack<Country> queueCustomers = new Stack<Country>();
-            queueCustomers.Push(country1);
-            queueCustomers.Push(country2);
-            queueCustomers.Push(country3);
-            queueCustomers.Push(country4);
-            queueCustomers.Push(country5);
-
-            queueCustomers.Pop();
-
-            foreach (Country c in queueCustomers)
-            {
-                Console.WriteLine(c.Name);
-            }
-
-
-            
-
+            Thread T1 = new Thread(new ThreadStart(Number.PrintNumbers));
+            T1.Start();
         }
     }
-     
-    public class Country
+
+    class Number
     {
-        public string Name { get; set; }
-        public string Code { get; set; }   
-        public string Capital { get; set; }
+        public static void PrintNumbers()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i.ToString());    
+            }
+        }
     }
 }
